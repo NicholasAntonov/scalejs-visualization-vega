1ea43864873b1f3fd2d0c0acfcbe35d02a3a478b
nicholasantonov <nicholasantonov@gmail.com>
2014-08-11 09:51:04 -0400

    Add timeout animation test

--- scalejs.visualization-vega/scalejs.visualization-vega/visualization.js
+++ scalejs.visualization-vega/scalejs.visualization-vega/visualization.js
@@ -93,11 +93,33 @@ define([
             ]
         };
 
+        var data2 = {
+            table: [
+              { "x": 1, "y": 28 }, { "x": 2, "y": 55 },
+              { "x": 3, "y": 43 }, { "x": 4, "y": 91 },
+              { "x": 5, "y": 81 }, { "x": 6, "y": 53 },
+              { "x": 7, "y": 19 }, { "x": 8, "y": 87 },
+              { "x": 9, "y": 52 }, { "x": 10, "y": 48 },
+              { "x": 11, "y": 24 }, { "x": 12, "y": 49 },
+              { "x": 13, "y": 87 }, { "x": 14, "y": 66 },
+              { "x": 15, "y": 17 }, { "x": 16, "y": 27 },
+              { "x": 17, "y": 68 }, { "x": 18, "y": 16 },
+              { "x": 19, "y": 49 }, { "x": 20, "y": 75 },
+              { "x": 21, "y": 60 }, { "x": 22, "y": 50 }
+            ]
+        };
+
         element.setAttribute("id", "vega-vis-div");//ERROR this only allows for one vis per page, fix later
 
         vg.parse.spec(vegaSpec, function (chart) {
             var view = chart({ el: "#vega-vis-div", data: data }).update();
         });
+
+        setTimeout(function () {
+            vg.parse.spec(vegaSpec, function (chart) {
+                view = chart({ el: "#vega-vis-div", data: data2 }).update({ duration: 500, ease: "bounce-in" });
+            });
+        }, 3000);
     }
 
     return {
